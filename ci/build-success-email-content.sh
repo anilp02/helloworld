3b8fcb19b1d03b00786572f31e815e75f5d357b2
pp010 <padarabinda.panigrahi@cognizant.com>
2017-08-23 12:24:55 +0530

    final commit

--- ci/build-success-email-content.sh
+++ ci/build-success-email-content.sh
@@ -1,9 +1,6 @@
 - -exc
 - |
 
-echo $(pwd)
-echo $(ls -lart)
-
 LAST_COMMIT_HASH=$(cd sourceCode && git log -1 | grep commit | cut -d' ' -f2)
 
 LAST_COMMIT_DETAILS=$(cd sourceCode && git log -1 --name-status)
@@ -11,21 +8,3 @@ LAST_COMMIT_DETAILS=$(cd sourceCode && git log -1 --name-status)
 echo "Build Successful ${LAST_COMMIT_HASH}" >> email-content/subject.txt
 
 echo "Build Successful\n\n ${LAST_COMMIT_DETAILS}" >> email-content/body.txt
-
-echo $(pwd)
-echo $(ls -lart)
-
-cd email-content
-
-echo $(pwd)
-echo $(ls -lart)
-
-value1=`cat subject.txt`
-echo "$value1"
-value2=`cat body.txt`
-echo "$value2"
-
-cd ..
-
-echo $(pwd)
-echo $(ls -lart)
--- ci/build.sh
+++ ci/build.sh
@@ -1,32 +1,17 @@
 - -exc
 - |
 
-echo $(pwd)
-echo $(ls -lart)
-
 cd sourceCode
-
-echo $(pwd)
-echo $(ls -lart)
 
 # mvn clean install -e -DskipTests=true
 
-#mvn clean install -e -DskipTests=false
+mvn clean install -e -DskipTests=false
 
-#mvn sonar:sonar -Dsonar.host.url=http://54.169.190.81:9000
+mvn sonar:sonar -Dsonar.host.url=http://54.169.190.81:9000
 
 cd target
 
-echo $(pwd)
-echo $(ls -lart)
-
-#mv *.jar ../../jarFile/*.jar
+mv *.jar ../../jarFile/*.jar
 cd ../../jarFile
 
-echo $(pwd)
-echo $(ls -lart)
-
 cd ..
-
-echo $(pwd)
-echo $(ls -lart)
