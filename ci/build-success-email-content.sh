6ce0ca9f1747f1109a944a01d29275df4087f153
pp010 <padarabinda.panigrahi@cognizant.com>
2017-08-23 11:49:12 +0530

    yml

--- ci/build-success-email-content.sh
+++ ci/build-success-email-content.sh
@@ -8,9 +8,9 @@ LAST_COMMIT_HASH=$(cd sourceCode && git log -1 | grep commit | cut -d' ' -f2)
 
 LAST_COMMIT_DETAILS=$(cd sourceCode && git log -1 --name-status)
 
-echo "Build Successful ${LAST_COMMIT_HASH}" >> email-content/subject.txt
+echo "Build Successful ${LAST_COMMIT_HASH}" >> email-content/subject.yml
 
-echo "Build Successful\n\n ${LAST_COMMIT_DETAILS}" >> email-content/body.txt
+echo "Build Successful\n\n ${LAST_COMMIT_DETAILS}" >> email-content/body.yml
 
 echo $(pwd)
 echo $(ls -lart)
