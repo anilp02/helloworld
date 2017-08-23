03dde8132104c1754c5fee012a5084db128bc810
pp010 <padarabinda.panigrahi@cognizant.com>
2017-08-23 15:49:19 +0530

    final

--- ci/deploy-fail-email-content.sh
+++ ci/deploy-fail-email-content.sh
@@ -1,6 +1,9 @@
 - -exc
 - |
 
+echo $(pwd)
+echo $(ls -lart)
+
 LAST_COMMIT_HASH=$(cd sourceCode && git log -1 | grep commit | cut -d' ' -f2)
 
 LAST_COMMIT_DETAILS=$(cd sourceCode && git log -1 --name-status)
